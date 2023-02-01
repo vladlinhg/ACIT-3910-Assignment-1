93b5d03fc864089930104a3e642605ce6bf097bd
vladlinhg <victor.lin.hg@gmail.com>
2023-02-01 00:49:36 -0800

    Update add_a_word.py

--- add_a_word.py
+++ add_a_word.py
@@ -1,4 +1,8 @@
+"""
+    a simple program connect to mysql server with python and able to change or add a word into the database
+"""
 import mysql.connector
+
 
 class Database:
     def __init__(self) -> None:
@@ -6,14 +10,21 @@ class Database:
         self.port = input("What port to connect to (3306): ")
         self.user = input("What user to connect with (root): ")
         self.password = input("What password to connect with: ")
-    
+
+
 class MyDB:
     def __init__(self, db: Database):
+        """
+            MyDB is a class represent instance of mysql.connector
+
+        Args:
+            db (Database): Database is the class stored information to login into specific MySQL server 
+        """    
         try:
             res = mysql.connector.connect(
-                host = str(db.hostname),
-                user = str(db.user),
-                password = str(db.password)
+                host=str(db.hostname),
+                user=str(db.user),
+                password=str(db.password)
             )
             print("Connect successfully.")
             mycursor = res.cursor()
@@ -23,7 +34,7 @@ class MyDB:
                 print(row)
             self.db = res
             self.cursor = self.db.cursor()
-        
+
         except mysql.connector.Error as err:
             print("Could not login to host with user/password provided.")
             print("Exiting.")
@@ -36,23 +47,23 @@ class MyDB:
         self.cursor.execute(sql, entry)
         self.cursor.fetchall()
         if self.cursor.rowcount == 0:
-            print("The word '" + word +"' was not found... adding")
+            print("The word '" + word + "' was not found... adding")
             sql = "INSERT INTO dictionary.word (word) VALUES (%s)"
             entry = (word, )
             self.cursor.execute(sql, entry)
             self.db.commit()
             print("Added '" + word + "' to the database")
         else:
-            print("Found '" + word +"' in the database")
+            print("Found '" + word + "' in the database")
             result = self.cursor.fetchall()
             for row in result:
                 print(row)
-            change = input("Change '" + word +"' to: ")
+            change = input("Change '" + word + "' to: ")
             sql = "UPDATE dictionary.word SET word = %s WHERE word = %s"
             entry = (change, word)
             self.cursor.execute(sql, entry)
             self.db.commit()
-            print("Changed '" + word +"' to '" + change +"' in the database")
+            print("Changed '" + word + "' to '" + change + "' in the database")
 
 
 def main():
@@ -60,7 +71,6 @@ def main():
     mydb = MyDB(db)
     mydb.add_or_change_a_word()
 
+
 if __name__ == "__main__":
     main()
-
-
